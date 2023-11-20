3c7330a560f9f68160f0a000550f9df6d6b46185
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-11-20 11:44:38 +0100

    fin a

--- a.py
+++ a.py
@@ -4,16 +4,9 @@ def max_area(height):
     left, right = 0, n - 1
 
     while left < right:
-        # Calcular el ancho del recipiente
         width = right - left
-
-        # Calcular la altura del recipiente (tomar la menor altura entre las dos líneas)
         h = min(height[left], height[right])
-
-        # Actualizar el área máxima si es necesario
         max_area = max(max_area, width * h)
-
-        # Mover los índices hacia el centro
         if height[left] < height[right]:
             left += 1
         else:
